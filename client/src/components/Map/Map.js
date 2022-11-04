3517ae47d20cdb6a63b0d38cb2f39af389e587eb
ae-michu <72276028+ae-michu@users.noreply.github.com>
2022-11-04 22:07:15 +0100

    layout

---
+++ client/src/components/Map/Map.js
@@ -0,0 +1,12 @@
+import React from "react";
+import css from './Map.module.css';
+
+function Map() {
+    return (
+        <div className={css.container}>
+            
+        </div>
+    )
+}
+
+export default Map;
