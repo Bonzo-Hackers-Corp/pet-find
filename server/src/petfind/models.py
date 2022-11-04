d610a5a2d7bf735ad06a0772b0709d25a804b232
miloszmat123 <myyylek@gmail.com>
2022-11-04 20:29:03 +0100

    Database Posts model

--- server/src/petfind/models.py
+++ server/src/petfind/models.py
@@ -11,3 +11,27 @@ class User(models.Model):
 
 class Task(models.Model):
     description = models.CharField(max_length=128, help_text="Task description")
+
+
+class Posts(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=30, help_text="Post title")
+    author = models.CharField(max_length=50, help_text="Username")
+    location = models.JSONField()
+    date_time = models.DateTimeField()
+    description = models.CharField(max_length=1000)
+    phone_number = models.BigIntegerField()
+    tags = models.JSONField(blank = True)
+    reward  =models.JSONField(blank = True)
+    
+class Comments(models.Model):
+    id = models.IntegerField(primary_key=True)
+    post_id = models.ForeignKey(Posts, related_name=id)
+    title = models.CharField(max_length=30, help_text="Post title")
+    author = models.CharField(max_length=50, help_text="Username")
+    location = models.JSONField()
+    date_time = models.DateTimeField()
+    description = models.CharField(max_length=1000)
+    phone_number = models.BigIntegerField()
+    tags = models.JSONField(blank = True)
+    reward  =models.JSONField(blank = True)
