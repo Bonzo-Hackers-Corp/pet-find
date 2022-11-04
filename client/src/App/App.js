1bde014c5e6e7d3912f0d882c36b07f97067e2ea
michal-pawlicki <michalpawlicki02@gmail.com>
2022-11-05 00:02:56 +0100

    feat: card and cardlist components

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import css from './App.module.css';
+import React from "react";
+import css from "./App.module.css";
 
 // components
-import TopBar from '../components/TopBar/TopBar';
-import Map from '../components/Map/Map';
-import BottomBar from '../components/BottomBar/BottomBar';
+import TopBar from "../components/TopBar/TopBar";
+import Map from "../components/Map/Map";
+import BottomBar from "../components/BottomBar/BottomBar";
+import CardList from "../components/CardList/CardList";
 
 function App() {
   return (
     <div className={css.app}>
       <TopBar />
       <Map />
+      <CardList />
       <BottomBar />
     </div>
   );
