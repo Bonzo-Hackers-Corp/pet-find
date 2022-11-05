e8093224f6ab0942b7f7ea8b434bcad79285a22d
ae-michu <72276028+ae-michu@users.noreply.github.com>
2022-11-05 05:12:46 +0100

    small fix

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -15,15 +15,12 @@ import NewPostOverlay from "../components/NewPostOverlay/NewPostOverlay";
 
 function App() {
   const [newPostOverlayVisible, setNewPostOverlayVisible] = useState(false);
-<<<<<<< HEAD
   const modalVisible = useSelector((state) => state.user.modalOpen);
-=======
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchPosts());
   }, [dispatch]);
->>>>>>> 72f0c56b476f65126a32c57819835368b0132b1e
 
   return (
     <div className={css.app}>
@@ -35,11 +32,7 @@ function App() {
         onClickNew={() => setNewPostOverlayVisible(true)}
       />
 
-<<<<<<< HEAD
-      {/* <NewPostOverlay visible={newPostOverlayVisible}/> */}
-=======
       <NewPostOverlay visible={newPostOverlayVisible} onCancel={() => setNewPostOverlayVisible(false)}/>
->>>>>>> 72f0c56b476f65126a32c57819835368b0132b1e
     </div>
   );
 }
