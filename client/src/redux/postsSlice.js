a636f0bc5c669f3e143f26c718362d6724a582ff
michal-pawlicki <michalpawlicki02@gmail.com>
2022-11-05 03:28:09 +0100

    getlist naprawa problemu

--- client/src/redux/postsSlice.js
+++ client/src/redux/postsSlice.js
@@ -1,29 +1,22 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { getList } from "../api/getList";
 
-export const fetchPosts = createAsyncThunk(
-    'posts/fetchPosts',
-    async () => {
-        const response = await getList();
-        console.log(response);
-    }
-);
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const response = await getList();
+  console.log(response);
+});
 
 export const postsSlice = createSlice({
-    name: 'posts',
-    initialState: {
-        posts: []
-    },
+  name: "posts",
+  initialState: {
+    posts: [],
+  },
 
-    reducers: {
-        
-    },
+  reducers: {},
 
-    extraReducers: (builder) => {
-        builder.addCase(fetchPosts.fulfilled, (state, actions) => {
-
-        });
-    }
+  extraReducers: (builder) => {
+    builder.addCase(fetchPosts.fulfilled, (state, actions) => {});
+  },
 });
 
 export default postsSlice.reducer;
