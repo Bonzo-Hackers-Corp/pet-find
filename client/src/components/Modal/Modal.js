c97aefd8eb49342af52299818c2915dce8e1a5a2
ae-michu <72276028+ae-michu@users.noreply.github.com>
2022-11-05 15:47:51 +0100

    quick fix

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import css from "./Modal.module.css";
-import Image from "./dog.jpg";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { setModalOpen } from "../../redux/userSlice.js";
@@ -53,7 +52,7 @@ const Modal = () => {
             <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z" />
           </svg>
         </button>
-        <img className={css.image} src={Image} alt="dog" />
+        <img className={css.image} src={postData.photo} alt="dog" />
         <div className={css.description}>
           <div className={css.title}>{postData.title}</div>
           <div className={css.info}>{postData.description}</div>
