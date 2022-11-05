07d4d21e896670bf5c06e2c9cc32ae6e08ba0813
michal-pawlicki <michalpawlicki02@gmail.com>
2022-11-05 01:45:28 +0100

    card animation

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -5,7 +5,9 @@ import Image from "./dog.jpg";
 const Card = (props) => {
   return (
     <div className={css.container}>
-      <img className={css.image} src={Image} alt="dog" />
+      <div className={css.test}>
+        <img className={css.image} src={Image} alt="dog" />
+      </div>
       <div className={css.description}>
         <div className={css.title}>{props.title}</div>
         <div className={css.info}>{props.description}</div>
